03f085c25349f07f2cdc33df11f664d2cfd465ba
Florian Zimmermann <flo.zimm@gmail.com>
2018-05-02 16:43:17 +0200

    Stop using .conf file

--- Makefile
+++ Makefile
@@ -4,11 +4,9 @@ all:
 install: rosmasterd.sh rosmasterd.service
 	cp rosmasterd.sh /usr/local/bin/
 	cp rosmasterd.service /etc/systemd/system/
-	cp rosmasterd.conf /etc/
 
 uninstall: /usr/local/bin/rosmasterd.sh /etc/systemd/system/rosmasterd.service
 	rm /usr/local/bin/rosmasterd.sh
 	rm /etc/systemd/system/rosmasterd.service
-	rm /etc/rosmasterd.conf
 
 .PHONY: all install uninstall
--- rosmasterd.sh
+++ rosmasterd.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
+sourcefile="/home/florian/catkin_ws/devel/setup.bash"
+launchargs="av_capture av_capture.launch"
+
 if [ "$(hostname -I)"="192.168.1.100" ]; then
-	echo "Assuming role of ROS master"
-	sourcefile="/opt/ros/kinetic/setup.bash"
-	if [ "$1" != ""]; then
-		sourcefile="$1"
-	fi
-	bash -c "source $sourcefile && roscore"
-	if [ "$2" != ""]; then
-		bash -c "source $sourcefile && roslaunch $1"
-	fi
+    echo "Assuming role of ROS master"
+    bash -c ". $sourcefile && roslaunch $launchargs"
 else
-	echo "Not assuming role of ROS master"
+    echo "Not assuming role of ROS master"
 fi
